c4f245411b87e190b72c1ae456d68695cfb1b971
kawinnat09 <Kwinnathmayura@gmail.com>
2022-07-11 16:47:56 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const http = require('http')
 const msg = require('./message.js')
+const {loadUser} = require('./user_models.js')
 
 const host = "localhost"
 const PORT = process.env.PORT || 5000
+
 const server = http.createServer((req, res) => {
+    const jsonContent = JSON.stringify(loadUser())
     res.writeHead(200, { 'Content-Type':'text/plain' })
     res.write(`${msg.message} I Like Football`)
     res.end()

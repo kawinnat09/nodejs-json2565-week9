a8f72f65e32ad03cd79a2fd973a4c6942e991e3c
kawinnat09 <Kwinnathmayura@gmail.com>
2022-07-11 17:21:00 +0700

    update

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const http = require('http')
 const msg = require('./message.js')
-const {loadUser} = require('./user_models.js')
+const {loadUser}  = require('./user_models')
+const {importVnl2022} = require('./data.json')
 
 const host = "localhost"
 const PORT = process.env.PORT || 5000
 
 const server = http.createServer((req, res) => {
-    const jsonContent = JSON.stringify(loadUser())
+    //const JsonContent = JSON.stringify(loadUser())
 
     res.writeHead(200, { 'Content-Type':'text/plain' })
-    res.write(`${jsonContent} I Like Football`)
+    //res.write(${JsonContent })
+    res.write(`${importVnl2022}`)
     res.end()
 })
 
